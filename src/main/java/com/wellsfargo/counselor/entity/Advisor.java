e639e5c0beb38aacc6688a308878d00e92d59a9d
Mexwell001 <161425389+Mexwell001@users.noreply.github.com>
2024-12-09 02:02:25 +0000

    Implemented data model entities

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,16 +1,16 @@
 package com.wellsfargo.counselor.entity;
 
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Advisor {
 
     @Id
-    @GeneratedValue()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long advisorId;
 
     @Column(nullable = false)
@@ -25,13 +25,14 @@ public class Advisor {
     @Column(nullable = false)
     private String phone;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
+    // Default constructor for JPA
     protected Advisor() {
-
     }
 
+    // Parameterized constructor to initialize all fields (excluding advisorId, as it is auto-generated)
     public Advisor(String firstName, String lastName, String address, String phone, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -39,6 +40,8 @@ public class Advisor {
         this.phone = phone;
         this.email = email;
     }
+
+    // Getter and Setter methods
 
     public Long getAdvisorId() {
         return advisorId;
@@ -82,5 +85,32 @@ public class Advisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    // Override toString() for easier debugging and logging
+    @Override
+    public String toString() {
+        return "Advisor{" +
+                "advisorId=" + advisorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    // Optionally, override equals() and hashCode() methods based on the email (unique)
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Advisor advisor = (Advisor) o;
+        return email != null && email.equals(advisor.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
     }
 }
